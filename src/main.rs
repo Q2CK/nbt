93e0a7bb7bc0bdb5971351afc219830a5ad86395
Q2CK <109765887+Q2CK@users.noreply.github.com>
2023-05-22 13:33:32 +0200

    versions

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::fs::{File, OpenOptions};
-use quartz_nbt::{self, io::Flavor};
+use quartz_nbt::{self, io::Flavor, NbtTag};
+
 
 fn main() {
     let mut file_in = File::open("my_cool_schematic.schem").unwrap();
@@ -14,10 +15,19 @@ fn main() {
         Flavor::GzCompressed)
         .unwrap();
 
+    let mut nbt_compound = quartz_nbt::NbtCompound::new();
+
+    nbt_compound.insert("BlockData", NbtTag::ByteArray(vec![0]));
+    nbt_compound.insert("BlockEntities", NbtTag::ByteArray(vec![]));
+    nbt_compound.insert("DataVersion", NbtTag::Short(2586));
+    nbt_compound.insert("Height", NbtTag::Byte(2));
+    nbt_compound.insert("Length", NbtTag::Byte(2));
+    nbt_compound.insert("Width", NbtTag::Byte(2));
+
     quartz_nbt::io::write_nbt(
         &mut file_out,
         Some("Schematic"),
-        &data.0,
+        &nbt_compound,
         Flavor::GzCompressed)
         .expect("TODO: panic message");
 
